bb220aa82b4e246ded507b3291bc4c4cb98b1523
i1-kmv <i1-kmv@mail.ru>
2022-08-04 20:09:32 +0300

    fix styles, create cartPage

---
+++ src/components/Cart-item.tsx
@@ -0,0 +1,39 @@
+import React, {FC} from "react"
+import plus from '../assets/img/plus.svg'
+
+
+
+export const CartItem:FC = () => {
+    return (
+        <div className="cart__item">
+            <div className="cart__item-img">
+                <img
+                    className="pizza-block__image"
+                    src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
+                    alt="Pizza"
+                />
+            </div>
+            <div className="cart__item-info">
+                <h3>Сырный цыпленок</h3>
+                <p>тонкое тесто, 26 см.</p>
+            </div>
+            <div className="cart__item-count">
+                <div className="button button--outline button--circle cart__item-count-minus">
+                    <img src={plus}/>
+                </div>
+                <b>2</b>
+                <div className="button button--outline button--circle cart__item-count-plus">
+                    <img src={plus}/>
+                </div>
+            </div>
+            <div className="cart__item-price">
+                <b>770 ₽</b>
+            </div>
+            <div className="cart__item-remove">
+                <div className="button button--outline button--circle">
+                   <img src={plus}/>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,10 +1,45 @@
-import React,{FC} from "react";
+import React, {FC} from "react"
+import cart from '../assets/img/cart.svg'
+import arrow from '../assets/img/grey-arrow-left.svg'
+import trash from '../assets/img/trash.svg'
+import {Link} from "react-router-dom";
+import {CartItem} from "../components/Cart-item";
 
 
-export const Cart:FC = () => {
-    return(
-        <>
-
-        </>
+export const Cart: FC = () => {
+    return (
+        <div className="content">
+            <div className="container container--cart">
+                <div className="cart">
+                    <div className="cart__top">
+                        <h2 className="content__title">
+                            <img src={cart}/> Корзина
+                        </h2>
+                        <div className="cart__clear">
+                            <img src={trash}/>
+                            <span>Очистить корзину</span>
+                        </div>
+                    </div>
+                    <div className="content__items">
+                        <CartItem/>
+                    </div>
+                    <div className="cart__bottom">
+                        <div className="cart__bottom-details">
+                            <span> Всего пицц: <b>3 шт.</b> </span>
+                            <span> Сумма заказа: <b>900 ₽</b> </span>
+                        </div>
+                        <div className="cart__bottom-buttons">
+                            <Link to="/" className="button button--outline button--add go-back-btn">
+                                <img src={arrow}/>
+                                <span>Вернуться назад</span>
+                            </Link>
+                            <div className="button pay-btn">
+                                <span>Оплатить сейчас</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
